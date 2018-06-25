d2cb5417293b369dd5e4074a2e32b40d56a9b8de
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-25 12:36:34 +1200

    creates most of the post routes

--- server/db/db.js
+++ server/db/db.js
@@ -3,5 +3,18 @@ const config = require(path.join(__dirname, '../../knexfile')).development
 const knex = require('knex')(config)
 
 module.exports = {
+  getPosts,
+  addPost
 }
 
+function getPosts () {
+  const db = knex
+  return db('posts').select()
+}
+
+function addPost (newPost) {
+  const db = knex
+  return db('posts').insert(newPost)
+}
+
+function update.
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,5 +7,35 @@ const router = express.Router()
 
 router.use(bodyParser.json())
 
+router.get('/', (req, res) => {
+  db.getPosts()
+    .then(posts => {
+      res.json(posts)
+    })
+    .catch(err => {
+      res.status(500).send('errrrrrrrrr: ' + err.message)
+    })
+})
+
+router.post('/', (req, res) => {
+  const newPost = req.body
+  db.addPost(newPost)
+    .then(post => {
+      res.status(200).end()
+    })
+    .catch(err => {
+      res.status(500).send('errrrrrrrrr: ' + err.message)
+    })
+})
 module.exports = router
 
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  db.updatePost(id)
+    .then(post => {
+      res.status(200).end()
+    })
+    .catch(err => {
+      res.status(500).send('errrrrrrrrr: ' + err.message)
+    })
+})
