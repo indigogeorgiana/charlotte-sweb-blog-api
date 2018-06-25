4e36fb1bf84f73cd950c2fe9494afdd9c9292971
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-25 12:41:03 +1200

    added get posts

--- server/db/db.js
+++ server/db/db.js
@@ -3,5 +3,11 @@ const config = require(path.join(__dirname, '../../knexfile')).development
 const knex = require('knex')(config)
 
 module.exports = {
+  getPosts
 }
 
+function getPosts (testDb) {
+  const db = testDb || knex
+  return db('Posts')
+    .select()
+}
